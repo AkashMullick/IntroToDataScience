53dea75d981caadc8d439d5386168cb13c790497
Akash Mullick <akashmullick1@gmail.com>
2020-04-23 11:45:16 -0400

    Style updates to ps2.py

--- pset2/ps2.py
+++ pset2/ps2.py
@@ -227,6 +227,7 @@ class StandardRobot(Robot):
     direction; when it would hit a wall, it *instead* chooses a new direction
     randomly.
     """
+
     def updatePositionAndClean(self):
         """
         Simulate the passage of a single time-step.
@@ -235,7 +236,7 @@ class StandardRobot(Robot):
         been cleaned.
         """
         next_position = self.getRobotPosition().getNewPosition(self.getRobotDirection(), self.speed)
-        if self.room.isPositionInRoom(next_position) == False:
+        if self.room.isPositionInRoom(next_position) is False:
             self.setRobotDirection(random.randint(0, 359))
         else:
             self.setRobotPosition(next_position)
@@ -295,6 +296,7 @@ class RandomWalkRobot(Robot):
     A RandomWalkRobot is a robot with the "random walk" movement strategy: it
     chooses a new direction at random at the end of each time-step.
     """
+
     def updatePositionAndClean(self):
         """
         Simulate the passage of a single time-step.
