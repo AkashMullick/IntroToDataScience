98e536f238da93cd23139c639de365dd671f5c70
Akash Mullick <akashmullick1@gmail.com>
2020-05-08 15:55:14 -0400

    Running and Analyzing a Simulation With a Drug

--- pset3/ProblemSet3/ps3b.py
+++ pset3/ProblemSet3/ps3b.py
@@ -457,13 +457,11 @@ def simulationWithDrug(numViruses, maxPop, maxBirthProb, clearProb, resistances,
                        mutProb, numTrials):
     """
     Runs simulations and plots graphs for problem 5.
-
     For each of numTrials trials, instantiates a patient, runs a simulation for
     150 timesteps, adds guttagonol, and runs the simulation for an additional
     150 timesteps.  At the end plots the average virus population size
     (for both the total virus population and the guttagonol-resistant virus
     population) as a function of time.
-
     numViruses: number of ResistantVirus to create for patient (an integer)
     maxPop: maximum virus population for patient (an integer)
     maxBirthProb: Maximum reproduction probability (a float between 0-1)        
@@ -475,5 +473,38 @@ def simulationWithDrug(numViruses, maxPop, maxBirthProb, clearProb, resistances,
     numTrials: number of simulation runs to execute (an integer)
     
     """
+    import numpy as np
+    
+    data = np.zeros(300)
+    data1 = np.zeros(300)
+    for i in range(numTrials):
+        virus = ResistantVirus(maxBirthProb, clearProb, resistances, mutProb)
+        viruses = [virus] * numViruses
+        patient = TreatedPatient(viruses, maxPop)
+        virus_count, resist_virus_count = [], []
+        for j in range(150):
+            patient.update()
+            virus_count.append(patient.getTotalPop())
+            resist_virus_count.append(patient.getResistPop(['guttagonol']))
+        patient.addPrescription('guttagonol')
+        for k in range(150):
+            patient.update()
+            virus_count.append(patient.getTotalPop())
+            resist_virus_count.append(patient.getResistPop(['guttagonol']))
+        data = data + virus_count
+        data1 = data1 + resist_virus_count
+    data_avg = data/numTrials
+    data1_avg = data1/numTrials
+    
+    pylab.plot(list([float('{0:.1f}'.format(i)) for i in data_avg]), label = r'Non-resistant population')
+    pylab.plot(list([float('{0:.1f}'.format(j)) for j in data1_avg]), label = r'Guttagonol Resistant population')
+    pylab.xlabel(r'Number of steps')
+    pylab.ylabel(r'Average Virus Population')
+    pylab.title(r'Virus Simulation in Patient')
+    pylab.legend()
+    pylab.show()
 
-    # TODO
+simulationWithDrug(100, 1000, 0.1, 0.05, {'guttagonol': False}, 0.005, 10)
+#simulationWithDrug(100, 1000, 0.9, 0.01, {'guttagonol': False}, 0.005, 10)
+#simulationWithDrug(100, 1000, 0.95, 0.1, {'guttagonol': False}, 0.1, 10)
+#simulationWithDrug(100, 1000, 0.1, 0.05, {'guttagonol': True}, 0.005, 10)
